262bb4bb621adc5e21fce54b8cc2c92c80f5c5bd
Nirvik Ghosh <97960335+Nirvik07@users.noreply.github.com>
2023-04-23 21:39:51 +0530

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -81,4 +81,4 @@ def generate_ticket():
     return render_template('form.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False ,host='0.0.0.0')
